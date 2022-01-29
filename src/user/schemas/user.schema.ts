d986d534e90877028226e61370fe23fd10bc8378
Malek Boubakri <malekbouba@gmail.com>
2022-01-29 15:20:26 +0100

    user schema init

---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,39 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop({ required: true, type: String })
+  name: string;
+
+  @Prop({ required: true, type: String })
+  email: string;
+
+  @Prop({ required: true, type: String })
+  password: string;
+
+  @Prop({ required: true, type: String })
+  phone: string;
+
+  @Prop({ required: true, type: [String] })
+  roles: string[];
+
+  @Prop({ required: true, type: String })
+  firstName: string;
+
+  @Prop({ required: true, type: String })
+  lastName: string;
+
+  @Prop(Date)
+  birthDay?: Date;
+
+  @Prop(String)
+  address?: string;
+
+  @Prop(String)
+  avatar?: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
