6e44ea7f75cf502c52720bfde3c3d57b4b4ed6ce
Malek Boubakri <malekbouba@gmail.com>
2022-01-29 14:57:59 +0100

    mongoose config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { SubjectModule } from './subject/subject.module';
 import { SessionModule } from './session/session.module';
@@ -8,6 +9,7 @@ import { SessionDateModule } from './session-date/session-date.module';
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/haptdb'),
     UserModule,
     SubjectModule,
     SessionModule,
