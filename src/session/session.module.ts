d4b534bcbaca86ac11e012b135eb5a473c6f7d75
Malek Boubakri <malekbouba@gmail.com>
2022-01-29 15:42:43 +0100

    wiring modules & models

--- src/session/session.module.ts
+++ src/session/session.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Session, SessionSchema } from './schemas/session.schema';
 import { SessionController } from './session.controller';
 import { SessionService } from './session.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Session.name, schema: SessionSchema }]),
+  ],
   controllers: [SessionController],
   providers: [SessionService],
 })
--- src/subject/subject.module.ts
+++ src/subject/subject.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { SubjectService } from './subject.service';
 import { SubjectController } from './subject.controller';
+import { Subject, SubjectSchema } from './schemas/subject.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Subject.name, schema: SubjectSchema }]),
+  ],
   providers: [SubjectService],
   controllers: [SubjectController],
 })
