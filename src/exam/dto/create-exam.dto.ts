5eda1417341ae34b16269f721454d15b7420f32e
Safa Youssef <safa.youssef4070@gmail.com>
2022-02-05 14:57:14 +0100

    exam

--- src/exam/dto/create-exam.dto.ts
+++ src/exam/dto/create-exam.dto.ts
@@ -1,8 +1,12 @@
 import { Subject } from '../../subject/schemas/subject.schema';
 import { ExamTest } from '../../exam-test/schemas/exam-test.schema';
+import { IsString, IsOptional } from 'class-validator';
 
 export class CreateExamDto {
+  @IsString()
+  @IsOptional()
   label: string;
+  @IsString()
   type: string;
   subject: Subject;
   examTests?: ExamTest[];
