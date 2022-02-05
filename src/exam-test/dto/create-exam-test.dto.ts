0bb0786b493b26c5150b28a7d76008f20732b40c
Karim Ferjaoui <ferjaouikarim6@gmail.com>
2022-02-05 14:53:48 +0100

    examtest

--- src/exam-test/dto/create-exam-test.dto.ts
+++ src/exam-test/dto/create-exam-test.dto.ts
@@ -1,11 +1,16 @@
+import { IsMongoId, IsOptional } from 'class-validator';
 import { Exam } from '../../exam/scheams/exam.schema';
 import { SessionDate } from '../../session-date/schemas/session-date.schema';
 import { User } from '../../user/schemas/user.schema';
 
 export class CreateExamTestDto {
+  @IsMongoId()
   exam: Exam;
+  @IsMongoId()
   sessionDate: SessionDate;
+  @IsOptional()
   score?: number;
+  @IsOptional()
   note?: string;
   student: User;
 }
--- src/exam-test/dto/update-exam-test.dto.ts
+++ src/exam-test/dto/update-exam-test.dto.ts
@@ -1,11 +1,18 @@
+import { IsNumberString, IsOptional } from 'class-validator';
 import Exam from 'src/exam/exam.interface';
 import SessionDate from 'src/session-date/session-date.interface';
 import User from 'src/user/user.interface';
 
 export class UpdateExamTestDto {
+  @IsOptional()
   exam?: Exam;
+  @IsOptional()
   sessionDate?: SessionDate;
+  @IsOptional()
   score?: number;
+  @IsNumberString()
+  @IsOptional()
   note?: string;
+  @IsOptional()
   student?: User;
 }
