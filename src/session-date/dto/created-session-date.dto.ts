6a6b1917b6c5357f4f581d1a61c9de1113ee1589
Bayrem Sayeh <bayremsayeh98@gmail.com>
2022-02-05 14:57:09 +0100

    validation session date

--- src/session-date/dto/created-session-date.dto.ts
+++ src/session-date/dto/created-session-date.dto.ts
@@ -1,4 +1,4 @@
-import { IsDate } from 'class-validator';
+import { IsDate, IsString } from 'class-validator';
 import { Session } from '../../session/schemas/session.schema';
 import { User } from '../../user/schemas/user.schema';
 
@@ -7,8 +7,12 @@ export class CreatedSessionDateDto {
   start_date: Date;
   @IsDate()
   end_date: Date;
+  @IsString()
   session: Session;
+  @IsString()
   attendees: User[];
+  @IsString()
   teacher: User;
+  @IsString()
   note?: string;
 }
--- src/session-date/dto/update-session-date.dto.ts
+++ src/session-date/dto/update-session-date.dto.ts
@@ -1,14 +1,20 @@
 import Session from 'src/session/session.interface';
 import SessionDate from 'src/session-date/session-date.interface';
-import { IsDate } from 'class-validator';
+import { IsDate, IsOptional } from 'class-validator';
 
 export class UpdateSessionDateDto {
   @IsDate()
+  @IsOptional()
   start_date?: Date;
   @IsDate()
+  @IsOptional()
   end_date?: Date;
+  @IsOptional()
   session?: Session;
+  @IsOptional()
   attendees?: SessionDate[];
+  @IsOptional()
   teacher?: SessionDate;
+  @IsOptional()
   note?: string;
 }
