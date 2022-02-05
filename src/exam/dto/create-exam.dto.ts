14554423e203d854b26299c3cf2aacc90466df9c
Safa Youssef <safa.youssef4070@gmail.com>
2022-02-05 15:01:52 +0100

    exam

--- src/exam/dto/create-exam.dto.ts
+++ src/exam/dto/create-exam.dto.ts
@@ -1,6 +1,6 @@
 import { Subject } from '../../subject/schemas/subject.schema';
 import { ExamTest } from '../../exam-test/schemas/exam-test.schema';
-import { IsString, IsOptional } from 'class-validator';
+import { IsString, IsOptional, IsMongoId } from 'class-validator';
 
 export class CreateExamDto {
   @IsString()
@@ -8,6 +8,8 @@ export class CreateExamDto {
   label: string;
   @IsString()
   type: string;
+  @IsMongoId()
   subject: Subject;
+  @IsMongoId()
   examTests?: ExamTest[];
 }
