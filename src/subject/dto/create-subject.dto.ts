25ff2ec71fff78b16224261d61acdd7a3edd54c3
Salem Lokmani <salemlokmani99@gmail.com>
2022-02-05 14:51:13 +0100

    Dto Subject

--- src/subject/dto/create-subject.dto.ts
+++ src/subject/dto/create-subject.dto.ts
@@ -1,7 +1,30 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreateSubjectDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(10)
+  @MinLength(2)
   label: string;
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(10)
+  @MinLength(2)
   field: string;
+  @IsNotEmpty()
+  @IsNumber()
   nb_hour: number;
+  @IsNotEmpty()
+  @IsNumber()
   price_hour: number;
+  @IsOptional()
+  @IsString()
   description: string;
 }
--- src/subject/dto/update-subject.dto.ts
+++ src/subject/dto/update-subject.dto.ts
@@ -1,7 +1,31 @@
+import {
+  IsNumber,
+  IsOptional, IsPositive,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class UpdateSubjectDto {
-  label?: string;
-  field?: string;
-  nb_hour?: number;
-  price_hour?: number;
-  description?: string;
+  @IsOptional()
+  @IsString()
+  @MaxLength(10)
+  @MinLength(2)
+  label: string;
+  @IsOptional()
+  @IsString()
+  @MaxLength(10)
+  @MinLength(2)
+  field: string;
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  nb_hour: number;
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  price_hour: number;
+  @IsOptional()
+  @IsString()
+  description: string;
 }
